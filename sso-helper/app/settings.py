81956425d17158c03c7e8ca38c900a06dc31be1a
Paul Rudin <paul@rudin.co.uk>
2020-03-23 17:17:42 +0000

    delete sso-helper (split to new repo)

--- sso-helper/app/settings.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseSettings
-
-
-class Settings(BaseSettings):
-    commento_secret = b''
-    commento_url = 'https://comments.xamaral.com'
-
-    class Config:
-        env_prefix = 'SSO_HELPER'
